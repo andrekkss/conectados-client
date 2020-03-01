4f26832840a8add554a191a5bc2028fcdba4dfdb
Kitano <andre.kitano.silva@gmail.com>
2020-02-29 23:21:16 -0300

    Added return type in getAllClient method

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,13 +1,14 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
 import UserModel from "../model/user.model";
+import { UserEntity } from '../database/entity/user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly appService: UserRepository) {}
 
   @Get()
-  getAllClient() {
+  getAllClient(): Promise<UserEntity[]> {
     return this.appService.findAll();
   }
 
