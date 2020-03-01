1417858a2370e8bc2f96612a93e62b62acc4e2a9
Kitano <andre.kitano.silva@gmail.com>
2020-02-29 23:18:31 -0300

    Updated jest and added mock's for module and repository

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,40 +2,40 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/config/app.module';
+import { UserRepository } from '../src/repository/user.repository';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
+  let userRepository = {
+    findAll: () => ['test']
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(UserRepository)
+      .useValue(userRepository)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('create a client using model and post method', async () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .type('form')
+      .send({ name: 'André Kitano da Silva', cpf: 123456 })
+      .expect(201)
+      .expect('André Kitano da Silva');
+  });
+
+  it('should get all users from get method', async () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello');
-  });
-
-  it('create a client using model and post method', () => {
-    return request(app.getHttpServer())
-        .post('/')
-        .type('form')
-        .send({ name: 'André Kitano da Silva', cpf: 123456 })
-        .expect(201)
-        .expect('André Kitano da Silva')
-  });
-
-  it('should get all users from get method', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-
+      .expect(userRepository.findAll());
   });
 
   afterAll(async () => {
